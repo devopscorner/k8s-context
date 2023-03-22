53d6c0ed411ade85aa87230eb83533548a3277af
Dwi Fahni Denni <dfdenni0912@gmail.com>
2023-03-22 13:05:24 +0700

    Finalize version 1.1.3

---
+++ __archived__/src-v1.1.3/features/context.go
@@ -0,0 +1,162 @@
+package features
+
+import (
+	"fmt"
+
+	"github.com/AlecAivazis/survey/v2"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/clientcmd/api"
+)
+
+type KubeConfig struct {
+	Files     []string
+	Merged    *api.Config
+	Overwrite bool
+}
+
+func (kc *KubeConfig) Load() error {
+	var configs []*api.Config
+	for _, file := range kc.Files {
+		loaded, err := clientcmd.LoadFromFile(file)
+		if err != nil {
+			return err
+		}
+		configs = append(configs, loaded)
+	}
+	merged, err := MergeConfigs(configs)
+	if err != nil {
+		return err
+	}
+	kc.Merged = merged
+	return nil
+}
+
+func (kc *KubeConfig) SaveToFile(file string) error {
+	return clientcmd.WriteToFile(*kc.Merged, file)
+}
+
+func MergeConfigs(configs []*api.Config) (*api.Config, error) {
+	newConfig := &api.Config{
+		Kind:       "Config",
+		APIVersion: "v1",
+		Clusters:   make(map[string]*api.Cluster),
+		AuthInfos:  make(map[string]*api.AuthInfo),
+		Contexts:   make(map[string]*api.Context),
+	}
+
+	for _, config := range configs {
+		for k, v := range config.AuthInfos {
+			newConfig.AuthInfos[k] = v
+		}
+		for k, v := range config.Clusters {
+			newConfig.Clusters[k] = v
+		}
+		for k, v := range config.Contexts {
+			newConfig.Contexts[k] = v
+		}
+	}
+	for _, config := range configs {
+		if config.CurrentContext != "" {
+			newConfig.CurrentContext = config.CurrentContext
+			break
+		}
+	}
+
+	return newConfig, nil
+}
+
+func GetClientSet(kubeconfig string) (*kubernetes.Clientset, error) {
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		return nil, err
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	return clientset, nil
+}
+
+func GetCurrentContext(config *api.Config) (string, error) {
+	if config == nil {
+		return "", fmt.Errorf("kubeconfig is nil")
+	}
+	if config.CurrentContext == "" {
+		return "", fmt.Errorf("no current context set in kubeconfig")
+	}
+	context, ok := config.Contexts[config.CurrentContext]
+	if !ok {
+		return "", fmt.Errorf("current context not found in kubeconfig: %s", config.CurrentContext)
+	}
+	return context.Cluster, nil
+}
+
+func ListContexts(kc *KubeConfig) error {
+	fmt.Println("Available contexts:")
+	currentContext, err := GetCurrentContext(kc.Merged)
+	if err != nil {
+		return err
+	}
+	for contextName := range kc.Merged.Contexts {
+		prefix := " "
+		if contextName == currentContext {
+			prefix = "*"
+		}
+		fmt.Printf("%s %s\n", prefix, contextName)
+	}
+	return nil
+}
+
+func SwitchContext(kc *KubeConfig) error {
+	if err := kc.Load(); err != nil {
+		return err
+	}
+
+	var contextNames []string
+	for contextName := range kc.Merged.Contexts {
+		contextNames = append(contextNames, contextName)
+	}
+
+	var selectedContext string
+	prompt := &survey.Select{
+		Message: "Select a context",
+		Options: contextNames,
+	}
+
+	if err := survey.AskOne(prompt, &selectedContext, survey.WithValidator(survey.Required)); err != nil {
+		return err
+	}
+
+	config := kc.Merged
+	_, ok := config.Contexts[selectedContext]
+	if !ok {
+		return fmt.Errorf("context not found: %s", selectedContext)
+	}
+
+	config.CurrentContext = selectedContext
+	if err := kc.SaveToFile(kubeconfig); err != nil {
+		return err
+	}
+
+	fmt.Printf("Switched to context: %s\n", selectedContext)
+
+	return nil
+}
+
+func ShowContext(kc *KubeConfig) error {
+	if err := kc.Load(); err != nil {
+		return err
+	}
+
+	currentContext, err := GetCurrentContext(kc.Merged)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Current context: %s\n", currentContext)
+
+	return nil
+}
---
+++ __archived__/src-v1.1.3/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/devopscorner/k8s-context/src/features"
+	"github.com/muesli/termenv"
+)
+
+func main() {
+	logoStyle := termenv.Style{}.Foreground(termenv.ANSIGreen)
+	appNameStyle := termenv.Style{}.Foreground(termenv.ANSIWhite).Bold()
+
+	fmt.Println(logoStyle.Styled(features.Logo))
+	fmt.Println("[[ ", appNameStyle.Styled(features.AppName), " ]] -", features.VERSION)
+	fmt.Println("=============================")
+	features.GetCommands()
+}
